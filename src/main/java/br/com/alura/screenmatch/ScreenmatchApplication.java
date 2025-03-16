3269a997d9718d55bca9a60ee23805c93cccaab9
duartegdm <gduarte.study@gmail.com>
2025-03-16 12:38:32 -0300

    Novas funcionalidades utilizando Lambdas e Streams.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.models.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String filme = "gilmore girls";
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("http://www.omdbapi.com/?apikey=d0a3b0a2&t="
-                + filme.replace(" ", "+"));
-        System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
+
+
     }
 }
